5291d4ab0eefffb284510677c3372accb945cfb1
88maxwell <maxwell.soltyk@mail.ru>
2018-03-29 17:03:23 +0300

    Delete dist, and change config of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ const config = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            // filename: path.join(paths., 'index.html'),
+            filename: path.join(paths.DIST, 'index.html'),
             template: path.join(paths.SRC, 'index.html'),
         }),
         new ExtractTextPlugin('style.bundle.css')
